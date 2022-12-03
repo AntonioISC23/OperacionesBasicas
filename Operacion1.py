925e8dc822fcef28755d37348c5706cd3a55f9c9
Antonio <huguitar4@gmail.com>
2022-12-02 23:36:27 -0600

    ultimo cambio antes de subirlo a github

--- Operacion1.py
+++ Operacion1.py
@@ -2,7 +2,5 @@ a=float(input("Dame un numero: "))
 b=float(input("Dame otro numero: "))
 
 area=a*b
-perimetro=2*a+2*b
 
 print("Area calculada: ",str(area)) 
-print("Perimetro calculado: ",str(perimetro)) 
